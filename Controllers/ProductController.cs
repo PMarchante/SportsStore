dc827076362ff27607b1d41c7b965dbd24da357c
Pedro Marchante <pmarc560@gmail.com>
2019-09-10 11:31:45 -0400

    Created controllers models and views

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public ViewResult List() => View(_repo.listOfProducts);
+    }
+}
---
+++ Models/FakeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeRepository : IProductRepository
+    {
+        public IQueryable<Product> listOfProducts => new List<Product>
+        {
+            new Product{ Name="Golf balls", Price=19.99M},
+             new Product{ Name="Black socks", Price=8.99M},
+              new Product{ Name="Tennis racket", Price=35.99M}
+        }.AsQueryable<Product>();
+    }
+}
